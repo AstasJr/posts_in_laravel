02eeb43ecad5678e979b9f1b45cd1ae406f25323
Stanislav Andrianov <sandrianov@tdera.ru>
2022-11-30 23:26:29 +0300

    Start project

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,9 +2,10 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\GoogleRecaptcha;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
+class  Kernel extends HttpKernel
 {
     /**
      * The application's global HTTP middleware stack.
@@ -63,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'r' => GoogleRecaptcha::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware("r")->group(function() {
+    Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
 });
