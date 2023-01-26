73f1bd06be6aadeabe738b978afd869dbe963498
Stanislav Andrianov <sandrianov@tdera.ru>
2023-01-27 01:47:34 +0300

    Auth controller - test

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function showRegisterForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email', 'string'],
+            'password' => ['required', 'confirmed']
+        ]);
+
+        dd(1);
+
+    }
+}
