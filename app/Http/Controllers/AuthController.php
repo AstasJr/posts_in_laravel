dc5473665cb5e0b2fa078975609b95ffeaf09dcd
Stanislav Andrianov <sandrianov@tdera.ru>
2023-02-28 17:09:04 +0600

    Auth and register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function logout()
+    {
+        auth('web')->logout();
+        return redirect(route('home'));
+    }
+
     public function showLoginForm()
     {
         return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => ['required', 'email', 'string'],
+            'password' => ['required']
+        ]);
+
+        if (auth('web')->attempt($data)) {
+            return redirect(route('home'));
+        }
+
+        return redirect(route('login'))->withErrors(['email' => 'Incorrect data']);
     }
 
     public function showRegisterForm()
@@ -18,12 +39,22 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $request->validate([
-            'email' => ['required', 'email', 'string'],
+        $data = $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'string', 'unique:users,email'],
             'password' => ['required', 'confirmed']
         ]);
 
-        dd(1);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
 
+        if ($user) {
+            auth("web")->login($user);
+        }
+
+        return redirect(route('home'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers;
 
 Route::middleware("r")->group(function() {
-    Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
+    Route::get('/', [Controllers\IndexController::class, 'index'])->name('home');
 });
 
-Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
+Route::get('/posts', [Controllers\PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{id}', [Controllers\PostController::class, 'show'])->name('posts.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [Controllers\AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [Controllers\AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/loginProcess', [Controllers\AuthController::class, 'login'])->name('loginProcess');
+
+    Route::get('/register', [Controllers\AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/registerProcess', [Controllers\AuthController::class, 'register'])->name('registerProcess');
+});
